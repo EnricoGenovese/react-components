f47d61b426e2381059942fb85eb6d47689015cf4
EnricoGenovese <enricogeno@libero.it>
2024-12-12 15:32:53 +0100

    add footer;

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,14 @@ function App() {
       <header className="text-center mt-2 mb-5">
         <h1>Il mio blog</h1>
       </header>
-      <main className="container mx-auto w-50 ">
+      <main className="container mx-auto w-50 mb-5">
         <CardComponent />
       </main>
+      <footer className="text-bg-dark text-center p-3">
+        <div className="container">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quae recusandae autem suscipit tempora dolore omnis aliquid commodi, ipsam eveniet, reiciendis quod quidem nam amet? Impedit reiciendis illum ratione ullam.</p>
+        </div>
+      </footer>
     </>
   )
 }
